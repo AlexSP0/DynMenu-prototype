bf707a06a4a8ada4fdd6618042ce3d90abb1cd5c
Alexey Saprunov <workernight@yandex.ru>
2023-05-23 14:17:14 +0400

    chore: add docs for Command class

--- mcore/command.h
+++ mcore/command.h
@@ -8,24 +8,56 @@
 
 #include <memory>
 
+/*!
+ * \brief Container for QAction object
+ *
+ * The container contains additional information about the QAction type object,
+ * such as ID and text.
+ *
+ */
+
 class Command : public QObject
 {
 public:
     Q_OBJECT
 public:
+    /*!
+    Creates container with specified QAction
+    \param[in] action pointer to QAction object
+    */
     Command(QAction *action);
 
+    /*!
+    Return the ID of container
+    \return ID of container
+    */
     QUuid getId();
 
+    /*!
+    Compares two containers by ID
+    \param[in] other reference to the container with which the comparison is made
+    \return returns true if the id of the containers is the same, false if they are different
+    */
     bool operator==(const Command &other);
 
+    /*!
+    Returns pointer to QAction object in this container
+    \return pointer to QAction object
+    */
     QAction *getAction();
 
+    /*!
+    Returns text in this container
+    \return Returns text
+    */
     QString &getText();
 
+    /*!
+    Sets the string for the container
+    \param[in] text string to be set
+    */
     void setText(QString text);
 
-    //TO DO hide all member and write getters
 private:
     QUuid m_id;
 
